8f231ab80fef4078c80061c8cbb9c47c11bc9936
Matt Birman <contact@mattbirman.net>
2016-11-11 17:49:58 +1100

    gitignore working

---
+++ Ecommerce/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("WebApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("B03D6C52-0196-416D-A9ED-2CAC0B03FC58")]
+
---
+++ EcommerceTests/Web/AddItemControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Moq;
+using Xunit;
+
+namespace EcommerceTests.Web
+{
+    public class AddItemControllerTests
+    {
+        public AddItemControllerTests()
+        {
+        }
+
+        [Fact]
+        public void GivenAnAddItemRequestIsValid_WhenAnAddItemRequestIsReceived_ThenAnAddItemCommandIsSent()
+        {
+            //Setup
+            var mockCommandBus = new Mock<ICommandBus>();
+            var addItemController = new AddItemController(mockCommandBus.Object);
+            var addItemRequest = new AddItemRequest();
+
+
+            //Act
+            addItemController.PostAction(addItemRequest);
+
+            //Test
+            mockCommandBus.Verify(x => x.Send(It.IsAny<AddItemCommand>()));
+        }
+    }
+
+    public interface ICommandBus
+    {
+        void Send(AddItemCommand @is);
+    }
+
+    public class AddItemCommand
+    {
+    }
+
+    public class AddItemRequest
+    {
+    }
+
+    public class AddItemController
+    {
+        private readonly ICommandBus _commandBus;
+
+        public AddItemController(ICommandBus commandBus)
+        {
+            _commandBus = commandBus;
+        }
+
+        public void PostAction(AddItemRequest addItemRequest)
+        {
+            _commandBus.Send(new AddItemCommand());
+        }
+    }
+}
